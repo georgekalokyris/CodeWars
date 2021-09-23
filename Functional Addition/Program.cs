c4914c7e99e2db4aac3e40966c3d7b35ec7c92bc
georgekalokyris <kalokirisg@gmail.com>
2021-09-23 16:44:22 +0100

    Functional Addition: Done

---
+++ Functional Addition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functional_Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.Add(1)(3));
+        }
+
+        
+        public static Func<double, double> Add(double n)
+        {
+            return x => x + n;
+        }
+
+    }
+}
