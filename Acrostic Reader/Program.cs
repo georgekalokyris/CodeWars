4ec607d0f751d9c5b98fe81f1ab2f4fd34cdabeb
georgekalokyris <kalokirisg@gmail.com>
2021-09-26 15:09:38 +0100

    Another easy-peasy

---
+++ Acrostic Reader/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acrostic_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReadOut(new string[] { "Jolly", "Amazing", "Courteous", "Keen" }));
+            Console.WriteLine(ReadOut(new string[] { "Marvelous", "Excellent", "Gifted" }));
+        }
+
+        public static string ReadOut(string[] acrostic) => new string(acrostic.Select(x => x[0]).ToArray());
+    }
+}
