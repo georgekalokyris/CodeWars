1e3ef323bfa456f11c347359bf03357539572a5f
georgekalokyris <kalokirisg@gmail.com>
2021-09-27 12:29:07 +0100

    CheckTheExam: checked!

---
+++ CheckTheExam/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/5a3dd29055519e23ec000074
+namespace CheckTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int CheckExam(string[] arr1, string[] arr2)
+        {
+            int score = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                score += arr1[i] == arr2[i] ? 4 : arr2[i] == "" ? 0 : -1;
+            }
+            return score < 0 ? score = 0 : score;
+        }
+    }
+}
