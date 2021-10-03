0255727376b61261492464cf671583215dddc687
georgekalokyris <kalokirisg@gmail.com>
2021-10-03 13:20:37 +0100

    Level 8 warm-up challenges :)

---
+++ EnumerableMagic25TakethefirstNelements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableMagic25TakethefirstNelements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Take(new int[] { 0, 1, 2, 3, 5, 8, 13 }, 3));
+            Console.WriteLine(Take(new int[] { 0, 1, 2, 3, 5, 8, 13 }, 0));
+            Console.WriteLine(Take(new int[] { }, 3));
+        }
+
+        public static int[] Take(int[] arr, int n) => arr.Take(n).ToArray();
+    }
+}
---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvenOrOdd(2));
+            Console.WriteLine(EvenOrOdd(1));
+            Console.WriteLine(EvenOrOdd(0));
+            Console.WriteLine(EvenOrOdd(7));
+            Console.WriteLine(EvenOrOdd(-1));
+        }
+
+        public static string EvenOrOdd(int number) => number % 2 == 0 ? "Even" : "Odd";
+    }
+}
