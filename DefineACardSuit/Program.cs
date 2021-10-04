e72a31cb1f3a6db0b416592489a5d25bc020e295
georgekalokyris <kalokirisg@gmail.com>
2021-10-04 12:12:25 +0100

    Ez

---
+++ DefineACardSuit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/5a360620f28b82a711000047/train/csharp
+namespace DefineACardSuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DefineSuit("10♣"));
+        }
+        public static string DefineSuit(string card)
+        {
+            switch (card[card.Length-1])
+            {
+                case '♣':
+                    return "clubs";
+                case '♦':
+                    return "diamonds";
+                case '♥':
+                    return "hearts";
+                case '♠':
+                    return "spades";
+                default:
+                    return "";
+            }
+        }
+    }
+}
