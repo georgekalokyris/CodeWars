f46a221567a943d8b66b8539d8bd147695b3be62
georgekalokyris <kalokirisg@gmail.com>
2021-10-07 17:48:53 +0100

    Weight is the number of melons really

---
+++ Watermelon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/55192f4ecd82ff826900089e/train/csharp
+namespace Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divide(2));
+            Console.WriteLine(Divide(3));
+            Console.WriteLine(Divide(4));
+            Console.WriteLine(Divide(5));
+            Console.WriteLine(Divide(6));
+        }
+
+        public static bool Divide(int weight) => weight % 2 == 0 && weight > 2 ? true : false;
+    }
+}
