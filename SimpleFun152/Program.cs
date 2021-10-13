ab75594313959ed106547bef766b473e91b1eba6
georgekalokyris <kalokirisg@gmail.com>
2021-10-13 10:18:10 +0100

    More challenges

---
+++ SimpleFun152/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/58acfe4ae0201e1708000075/train/csharp
+namespace SimpleFun152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(InviteMoreWomen(new int[]{1,-1,1}));
+            Console.WriteLine(InviteMoreWomen(new int[]{1,1,1}));
+            Console.WriteLine(InviteMoreWomen(new int[]{-1,-1,-1}));
+            Console.WriteLine(InviteMoreWomen(new int[]{1,-1}));
+        }
+
+        public static bool InviteMoreWomen(int[] L)
+        {
+            int women = L.Where(x => x == -1).Count();
+            int men = L.Where(x => x == 1).Count();
+            return men > women ? true : false;
+        }
+    }
+}
---
+++ SumOfDigits-DigitalRoot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits_DigitalRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitalRoot(16));
+        }
+
+        public static int DigitalRoot(long n)
+        {
+            if (n <= 9)
+            {
+                Int32.TryParse(n.ToString(), out int final);
+                return final;
+            }
+            else
+            {
+                char[] chArr = n.ToString().ToArray();
+                int[] intArr = new int[chArr.Length];
+                for (int i = 0; i < chArr.Length; i++)
+                {
+                    intArr[i] = int.Parse(chArr[i].ToString());
+
+                }
+                return DigitalRoot(intArr.Sum());
+            }
+        }
+
+        public static double Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);//Recursive call
+        }
+    }
+}
