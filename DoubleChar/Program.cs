53e632f2396e449852aafac7b20963be06272e02
georgekalokyris <kalokirisg@gmail.com>
2021-10-19 13:13:04 +0100

    Interesting one

---
+++ DoubleChar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/56b1f01c247c01db92000076/train/csharp
+namespace DoubleChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DoubleChar("illuminati"));
+        }
+
+        public static string DoubleChar(string s)=> string.Concat(s.Select(x => new string(x, 2)));
+    }
+
+
+}
