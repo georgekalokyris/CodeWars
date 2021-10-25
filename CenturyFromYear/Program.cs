5cefaa0803e751779d00c7c9ffd179280e39d940
georgekalokyris <kalokirisg@gmail.com>
2021-10-25 22:10:39 +0100

    Century calculation - 8kyo

---
+++ CenturyFromYear/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/csharp
+namespace CenturyFromYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Сentury(1705)); //18
+            Console.WriteLine("--------------");
+            Console.WriteLine(Сentury(1900)); //19
+            Console.WriteLine("--------------");
+            Console.WriteLine(Сentury(1601)); //17
+            Console.WriteLine("--------------");
+            Console.WriteLine(Сentury(2000)); //20
+        }
+
+        public static int Сentury(int year)
+        {
+            return (year / 100) + ((year % 100 == 0) ? 0 : 1);
+        }
+    }
+}
