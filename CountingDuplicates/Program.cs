1a4065ba0deed4b9181cf177fb868a486ab75f21
georgekalokyris <kalokirisg@gmail.com>
2021-11-02 21:26:57 +0000

    Good example for getting the duplicates from a string with Linq

---
+++ CountingDuplicates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(DuplicateCount("abcde"));
+            Console.WriteLine(DuplicateCount("aabbcde"));
+            Console.WriteLine(DuplicateCount("aabBcde"));
+        }
+
+        public static int DuplicateCount(string str)
+        {
+
+            var result = str.GroupBy(x => char.ToUpper(x)).Where(y=>y.Count() > 1).Select(c=>c.Key);
+
+            return result.Count();
+
+        }
+    }
+}
