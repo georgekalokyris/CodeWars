4382c3c911a46dc3d91129e33726766ff194aa52
georgekalokyris <kalokirisg@gmail.com>
2021-11-02 10:04:34 +0000

    Handy filtering

---
+++ FilterTheNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/55b051fac50a3292a9000025/train/csharp
+namespace FilterTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(FilterString("a123fb"));
+        }
+        public static int FilterString(string s)
+        {
+            return int.Parse(string.Concat(s.Where(x => char.IsNumber(x))));
+            //or just     return int.Parse(new String(s.Where(Char.IsDigit).ToArray()));
+        }
+
+    }
+}
