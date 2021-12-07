ffc9a854f17acdab6fca40db2f6e9b40a570574a
georgekalokyris <kalokirisg@gmail.com>
2021-12-07 14:25:55 +0000

    813kyo

---
+++ BitCounting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/526571aae218b8ee490006f4/train/csharp
+namespace BitCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountBits(0));
+            Console.WriteLine(CountBits(4));
+            Console.WriteLine(CountBits(7));
+            Console.WriteLine(CountBits(9));
+            Console.WriteLine(CountBits(10));
+
+        }
+        public static int CountBits(int n)
+        {
+            return Convert.ToString(n, 2).Count(x => int.Parse(x.ToString()) == 1);
+        }
+
+    }
+}
---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//https://www.codewars.com/kata/55f2b110f61eb01779000053/train/csharp
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSum(0, 1));
+            Console.WriteLine(GetSum(0, -1));
+
+            Console.WriteLine(string.Join("|",Enumerable.Range(0, 2)));
+        }
+
+        public static int GetSum(int a, int b)
+        {
+            return a == b ? a : a < b ? Enumerable.Range(a, b-a+1).Sum() : Enumerable.Range(b, a-b+1).Sum();
+        }
+    }
+}
